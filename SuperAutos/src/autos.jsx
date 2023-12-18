398c0302157680ff56229ed5f4c09821ab9af148
Ian <equablehorse2345@gmail.com>
2023-12-18 12:10:30 -0600

    Ya funciona con JSON y React
    Cambie el modo en el que se usaba el JSON para poder importar los datos. Las imagenes ahora se cargan desde /src/assets en vez de cargarse desde la raiz

--- SuperAutos/src/autos.jsx
+++ SuperAutos/src/autos.jsx
@@ -1,51 +1,49 @@
 
 import React from 'react';
-import axios from 'axios';
+import axios, { } from 'axios';
+import data from './datos.json'
 function AutoMain() {
     const cargarCoches = async (evento)=>{
+        
         console.log('cargar coche inicio')
-        //evento.preventDefault()
-        //await axios.get('http://localhost:4567/autos/')
-        await fetch('data.json')
-        .then(response => response.json())
-        .then(respuesta =>{
-        //.then(function (respuesta) {
-            console.log("datos en response ",respuesta.data);
+        
+        try {
+            //console.log("datos en response ",data);
             var i = 1;
-            const vehiculos = respuesta.data;
+            const vehiculos = data.vehiculos;
             const divisionVehiculos = document.getElementById('vehiculos')
             vehiculos.forEach(coche => {
-                console.log('el contador va en: ',i)
+                //console.log('el contador va en: ',i)
                 const tarjetaVehiculo = document.createElement('div');
                 tarjetaVehiculo.classList.add('tarjeta-vehiculo');
+                //console.log(coche.imagen)
                 tarjetaVehiculo.innerHTML = 
                 `
                     <p class="puertas">${coche.puertas}</p>
-                    <img src="${coche.imagen}" alt="${coche.nombre}">
+                    <img src="/src/assets/${coche.imagen}">
                     <p class="nombre">${coche.nombre}</p>
                     <p class="precio">$${coche.precio}</p>
                 `;
                 i++;
                 divisionVehiculos.appendChild(tarjetaVehiculo);
             });
+            console.log("cargar coches finalizado")
+        } catch (error) {
+            console.error(error)
+        }
+        
             
-            return(respuesta.data);
-            
-        })
-        .catch(function(error){
-            console.log(error);
-            
-        });   
+    
     }
     return(
 
     <>
-        <body onMouseOver={cargarCoches}>
+        <div onLoad={cargarCoches}>
             <div className="top"></div>
-            <h1><img src="banner.png" alt="Southernsanandreassuperautos.com"/></h1>
+            <h1><img src="/src/banner.png" alt="Southernsanandreassuperautos.com"/></h1>
             <div id="vehiculos"></div>
             
-        </body>
+        </div>
     </>
 
     )
